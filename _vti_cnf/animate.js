d0558bc56879e5bb0ee9e51f33d77d72c684607c
CWPS-Web <176980613+CWPS-Web@users.noreply.github.com>
2024-08-01 13:46:29 +0800

    Add files via upload

---
+++ _vti_cnf/animate.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|123-86F76532BE0\\123
+vti_modifiedby:SR|123-86F76532BE0\\123
+vti_timelastmodified:TR|19 Dec 2010 02:50:55 -0000
+vti_timecreated:TR|19 Dec 2010 02:50:55 -0000
+vti_lineageid:SR|{011B5592-0D11-448A-A213-BBE8AFC1FA6D}
+vti_cacheddtm:TX|19 Dec 2010 02:50:55 -0000
+vti_filesize:IR|16040
+vti_extenderversion:SR|5.0.2.2623
+vti_backlinkinfo:VX|993.htm 992.htm 99.htm 991.htm 997.htm 996.htm 995.htm 994.htm
